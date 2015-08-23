ef38c9854cd9c9d47e91a3ffde6cd2661afc8de8
Lin314159 <danielamdurer@gmail.com>
2015-08-23 15:36:25 +0100

    Reorganised code. fixed bugs. New release.

--- indexer.py
+++ indexer.py
@@ -1,46 +1,41 @@
-#############################
-#   Edit these variables    #
-#############################
-group = 'version'
-place = 'DVD'
-thing = 'distro'
-indexfile = '/home/daniel/distros'
-#####################################################
-#   Don't edit, unless you know what you are doing  #
-#####################################################
-import sys
-index = open(indexfile,'r+')
-def listall():
+#############################################################################
+#   Unless you know Python 3, only edit the arguments to the last function  #
+#############################################################################
+def program(thing, group, place, indexfile):
+    global index
+    index = open(indexfile,'r+')
+    mainprogram(thing, group, place, indexfile)
+def listall(thing, group, place):
     index.seek(0)
     for line_list in index:
-        displayline(line_list)
-def displayline(line):
+        displayline(line_list, thing, group, place)
+def displayline(line, thing, group, place):
     linelist=line.split('^')
-    print(linelist[0] + " with " + group + ' ' + linelist[1] + " is on " + place + ' ' + linelist[2])
-def listthing():
+    print(linelist[0] + " with/by " + group + ' ' + linelist[1] + " is on " + place + ' ' + linelist[2])
+def listthing(thing, group, place):
     thing_tmp = input("Which " + thing + " are you looking for?   ")
     index.seek(0)
     for line_thing in index:
         if thing_tmp in line_thing:
-            displayline(line_thing)
-def listplace():
+            displayline(line_thing, thing, group, place)
+def listplace(thing, group, place):
     place_tmp = input("Which " + place + " are you looking for?   ")
     index.seek(0)
     for line_place in index:
         if place_tmp in line_place:
-            displayline(line_place)
-def listgroup():
+            displayline(line_place, thing, group, place)
+def listgroup(thing, group, place):
     group_tmp = input("Which " + group + " are you looking for?   ")
     index.seek(0)
     for line_group in index:
         if group_tmp in line_group:
-            displayline(line_group)
-def add():
+            displayline(line_group, thing, group, place)
+def add(thing, group, place):
     thing_tmp_add = input("Which " + thing + " are you adding?   ")
     place_tmp_add = input("Which " + place + " is it on?  ")
     group_tmp_add = input("Which " + group + " is it (by)?  ")
-    index.write(thing_tmp_add + "^" + place_tmp_add + "^" + group_tmp_add)
-def delete():
+    index.write(thing_tmp_add + "^" + place_tmp_add + "^" + group_tmp_add + "\n")
+def delete(thing, group, place):
     thing_tmp_rm = input("Which " + thing + " are you deleting?   ")
     place_tmp_rm = input("Which " + place + " is it on?   ")
     group_tmp_rm = input("Which " + group + " is it (by)?   ")
@@ -48,7 +43,7 @@ def delete():
     index.seek(0)
     for line_rm in index:
         if thing_tmp_rm in line_rm and place_tmp_rm in line_rm and group_tmp_rm in line_rm:
-            displayline(line_rm)
+            displayline(line_rm, thing, group, place)
             lines_to_delete.append(line_rm)
         response_rm = input("Are you SURE you want to delete these lines?   ").upper()
         response_rm = response_rm[:1]
@@ -58,13 +53,16 @@ def delete():
             for i in lines_rm:
                 if i not in lines_to_delete:
                     index.write(i)
-def reset():
+def reset(thing, group, place, indexfile):
+    import os
     response_wipe = input("Are you SURE you want to wipe the database?   ").upper()
     response_wipe = response_wipe[:1]
     if response_wipe == "Y":
-        index.seek(0)
-        index.write('')
-def mainprogram():
+        index.close()
+        os.remove(indexfile)
+        os.mknod(indexfile)
+        exit()
+def mainprogram(thing, group, place, indexfile):
     while True:
         print("What type of operation do you want to do?")
         print("1: read-only operations")
@@ -72,51 +70,56 @@ def mainprogram():
         print("q: quit")
         choice_main = input()
         if choice_main == '1':
-            ro()
+            ro(thing, group, place)
         elif choice_main == '2':
-            rw()
+            rw(thing, group, place, indexfile)
         elif choice_main == 'q':
             index.close()
-            sys.exit()
+            exit()
         else:
             print("Input 1 or 2!")
-def ro():
+def ro(thing, group, place):
     while True:
         print("Which operation do you want to do?")
         print("1: list all " + thing +"s")
         print("2: find information about a/an " + thing)
         print("3: list everything with/by a specific " + group)
-        print("4: list everything on a " + place)
-        print("q: quit")
+        print("4: list everything on a/an " + place)
+        print("b: back")
         choice_ro = input()
         if choice_ro == '1':
-            listall()
+            listall(thing, group, place)
         elif choice_ro == '2':
-            listthing()
+            listthing(thing, group, place)
         elif choice_ro == '3':
-            listgroup()
+            listgroup(thing, group, place)
         elif choice_ro == '4':
-            listplace()
-        elif choice_ro == 'q':
+            listplace(thing, group, place)
+        elif choice_ro == 'b':
             break
         else:
             print("Input 1-4!")
-def rw():
+def rw(thing, group, place, indexfile):
     while True:
         print("Which operation do you want to do?")
-        print("1: add a " + thing)
-        print("2: remove a " + thing)
+        print("1: add a/an " + thing)
+        print("2: remove a/an " + thing)
         print("3: wipe the database")
-        print("q: quit")
+        print("b: back")
         choice_rw = input()
         if choice_rw == '1':
-            add()
+            add(thing, group, place)
         elif choice_rw == '2':
-            delete()
+            delete(thing, group, place)
         elif choice_rw == '3':
-            reset()
-        elif choice_rw == 'q':
+            reset(thing, group, place, indexfile)
+        elif choice_rw == 'b':
             break
         else:
             print("Input 1-3!")
-mainprogram()
+#####################################################
+#               NO EDITING ABOVE HERE               #
+#####################################################
+#                   EDIT BELOW HERE                 #
+#####################################################            
+program('song', 'artist', 'album', '/home/daniel/foo')
