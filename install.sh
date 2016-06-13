ddfd586579df53ee22e5f357f872620f8d38d74b
Lin314159 <danielamdurer@gmail.com>
2016-06-13 11:59:40 +0100

    Edited install script

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
 mkdir ~/.config/indexer
 cp indexer.conf ~/.config/indexer/ && chmod 644 ~/.config/indexer/indexer.conf
 sudo cp indexer.py /usr/local/bin/ && sudo chmod 755 /usr/local/bin/indexer.py
+sudo cp indexer /usr/local/bin && sudo chmod 755 /usr/local/bin/indexer
+
